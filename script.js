1924f237bdd2ac590ef74ae28400bde5c3f1b95c
Ashley Kung <ashley.berg@gmail.com>
2019-07-09 20:22:36 -0400

    Clean up code

--- script.js
+++ script.js
@@ -11,9 +11,6 @@ function multiply(x,y) {
 }
 
 function divide(x,y) {
-  if (y === 0) {
-    return 'CAN\'T DIVIDE BY ZERO FOOL!'
-  }
   return parseFloat(x) / parseFloat(y);
 }
 
@@ -27,11 +24,8 @@ function operate(operator, x, y) {
 //button-click event listeners
 let calculator = document.getElementById('calculator');
 let display = document.getElementById('display');
-let values = [];
 let operator = '';
 let result = 0;
-let numsPressed = 0;
-let operatorsPressed = 0;
 let firstNum = '';
 let secondNum = '';
 let previousKeyType = '';
@@ -39,47 +33,43 @@ calculator.addEventListener('click', function(event) {
 	let displayedNum = display.textContent;
   if (!isNaN(parseInt(event.target.id))) {
 	  if (displayedNum === '0' || previousKeyType === 'operator' || previousKeyType === 'equals') {
-//      values.push(event.target.id);
       display.textContent = event.target.id;
-	  previousKeyType = 'number';
+	    previousKeyType = 'number';
 	  } else {
 		  display.textContent = displayedNum + event.target.id;
 	  }
   }
   if (event.target.id === 'clear') {
-	previousKeyType = '';
+	  previousKeyType = '';
     display.textContent = '0';
-    values = [];
     result = 0;
     operator = '';
-    numsPressed = 0;
-	firstNum = '';
-	secondNum = '';
+  	firstNum = '';
+	  secondNum = '';
     return;
   }
   if (event.target.id === '.') {
-	if (!displayedNum.includes('.') && previousKeyType !== 'equals') {
-	  display.textContent = displayedNum + '.'
-	} else if (previousKeyType === 'operator' || previousKeyType === 'equals') {
-		display.textContent = '0.';
-	}
-		  previousKeyType = '';
+	  if (!displayedNum.includes('.') && previousKeyType !== 'equals') {
+	    display.textContent = displayedNum + '.'
+	  } else if (previousKeyType === 'operator' || previousKeyType === 'equals') {
+		  display.textContent = '0.';
+	  }
+		previousKeyType = '';
   }
   if (event.target.id === '*') {
-	  	  if (firstNum && operator && previousKeyType !== 'operator' && previousKeyType !== 'equals') {
+	  if (firstNum && operator && previousKeyType !== 'operator' && previousKeyType !== 'equals') {
 		  display.textContent = operate(operator, firstNum, display.textContent);
 	  }
     operator = '*';
-	previousKeyType = 'operator';
+	  previousKeyType = 'operator';
     firstNum = display.textContent;
-	
   }
   if (event.target.id === '/') {
-	  	  if (firstNum && operator && previousKeyType !== 'operator' && previousKeyType !== 'equals') {
+	  if (firstNum && operator && previousKeyType !== 'operator' && previousKeyType !== 'equals') {
 		  display.textContent = operate(operator, firstNum, display.textContent);
 	  }
     operator = '/';
-	previousKeyType = 'operator';
+	  previousKeyType = 'operator';
     firstNum = display.textContent;
   }
   if (event.target.id === '+') {
@@ -87,29 +77,25 @@ calculator.addEventListener('click', function(event) {
 		  display.textContent = operate(operator, firstNum, display.textContent);
 	  }
     operator = '+';
-	previousKeyType = 'operator';
+	  reviousKeyType = 'operator';
     firstNum = display.textContent;
-	
   }
   if (event.target.id === '-') {
-	  	  if (firstNum && operator && previousKeyType !== 'operator' && previousKeyType !== 'equals') {
+	  if (firstNum && operator && previousKeyType !== 'operator' && previousKeyType !== 'equals') {
 		  display.textContent = operate(operator, firstNum, display.textContent);
 	  }
     operator = '-';
-	previousKeyType = 'operator';
+	  previousKeyType = 'operator';
     firstNum = display.textContent;
   }
   if (event.target.id === '=') {
 	  if (previousKeyType === 'equals') {
 	  } else {
 	  previousKeyType = 'equals';
-//	secondNum = display.textContent;
     result = operate(operator, firstNum, display.textContent);
- //   console.log(result);
-	 display.textContent = result;
-	firstNum = result;
-	operator = '';
-    //firstNum = result;
+	  display.textContent = result;
+	  firstNum = result;
+	  operator = '';
 	  }
-}
+  }
 });
